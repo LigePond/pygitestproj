d6112c60a5353bcee9a1d153b18abde4d3137a38
Volkov Aleksei <rohtuasad@gmail.com>
2023-12-05 23:08:44 +0300

    Added division operation Closes #1

--- calc.py
+++ calc.py
@@ -14,10 +14,11 @@ print("Выберите операцию:")
 print("1. Сложение")
 print("2. Вычитание")
 print("3. Умножение")
+print("4. Деление")
 
-choice = input("Введите номер операции (1/2/3): ")
+choice = input("Введите номер операции (1/2/3/4): ")
 
-if choice in ('1', '2', '3'):
+if choice in ('1', '2', '3', '4'):
     if choice == '1':
         result = add(num1, num2)
         print(f"{num1} + {num2} = {result}")
@@ -27,5 +28,11 @@ if choice in ('1', '2', '3'):
     elif choice == '3':
         result = multiply(num1, num2)
         print(f"{num1} * {num2} = {result}")
+    elif choice == '4':
+        if num2 == 0:
+            print("Деление на ноль запрещено.")
+            exit()
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
 else:
     print("Некорректный ввод операции.")
